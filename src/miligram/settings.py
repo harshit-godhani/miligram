cb17b177734f65cb221d72af52294665bb4a00aa
harshitgodhani07 <153496386+harshitgodhani07@users.noreply.github.com>
2024-05-29 10:11:15 +0530

    change database

--- src/miligram/settings.py
+++ src/miligram/settings.py
@@ -86,18 +86,18 @@ DATABASES = {
 }
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # },
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
-    "new": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgresql",
+        "NAME": "postgres",
         "USER": "postgres",
         "PASSWORD": "1234",
         "HOST": "localhost",
         "PORT": "5432",
-    },
+    }
 }
 
 
