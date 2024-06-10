e8768164c862f0b67eb3ee1084aca9edc9b20450
Harshit Godhani <153496386+harshit-godhani@users.noreply.github.com>
2024-06-10 10:26:43 +0530

    sqllite database update

--- src/miligram/settings.py
+++ src/miligram/settings.py
@@ -78,18 +78,18 @@ WSGI_APPLICATION = "miligram.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # },
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "1234",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    },
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": "postgres",
+    #     "USER": "postgres",
+    #     "PASSWORD": "1234",
+    #     "HOST": "localhost",
+    #     "PORT": "5432",
+    # }
 }
 
 
